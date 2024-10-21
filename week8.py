1696a6e51a6d99cfe09ca0d40ded14ca94e67732
DestroyerX7 <Bojera22@gmail.com>
2024-10-20 22:29:36 -0600

    week 8

---
+++ week8.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, num, leftNode, rightNode):
+        self.leftNode = leftNode
+        self.rightNode = rightNode
+        self.num = num
+
+    def add(self, node):
+        if node.num > self.num:
+            if self.rightNode is None:
+                self.rightNode = node
+            else:
+                self.rightNode.add(node)
+        elif node.num < self.num:
+            if self.leftNode is None:
+                self.leftNode = node
+            else:
+                self.leftNode.add(node)
+        else:
+            print("Duplicate num not added")
+
+    def print(self):
+        if self.leftNode is not None:
+            self.leftNode.print()
+        
+        print(self.num)
+
+        if self.rightNode is not None:
+            self.rightNode.print()
+
+root = Node(50, None, None)
+root.add(Node(60, None, None))
+root.add(Node(40, None, None))
+root.add(Node(10, None, None))
+root.add(Node(80, None, None))
+root.add(Node(100, None, None))
+root.add(Node(70, None, None))
+root.add(Node(0, None, None))
+root.add(Node(20, None, None))
+root.add(Node(30, None, None))
+root.add(Node(90, None, None))
+root.print()
